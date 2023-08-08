d92aad483f2057a0fa99ad72ae0f263aafd8bc1a
Djemm20 <105044342+Djemm20@users.noreply.github.com>
2023-08-08 11:07:02 +0000

    Add files via upload

--- esp.py
+++ esp.py
@@ -1,22 +1,27 @@
+# import necessary libraries
 import re
 import pandas as pd
+from matplotlib import pyplot as plt
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
+# read dataset
 data = pd.read_csv('data.csv')
 
+# combine features
 data['text_sub'] = data['text'] + ' ' + data['subreddit']
 
+# assign feature and target variable
 X = data['text_sub']
 y = data['label']
 
 lemmatizer = WordNetLemmatizer()
 
-
+# preprocess dataset
 def preprocess_dataset(dataset):
     preprocessed_data = []
     for sentence in dataset:
@@ -36,12 +41,12 @@ X = preprocess_dataset(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 # learn data conversion to binary
-cv = TfidfVectorizer(binary=True)
-cv.fit_transform(X_train)
+vectorizer = TfidfVectorizer(binary=True)
+vectorizer.fit_transform(X_train)
 
 # convert features to binary
-X_train = cv.transform(X_train)
-X_test = cv.transform(X_test)
+X_train = vectorizer.transform(X_train)
+X_test = vectorizer.transform(X_test)
 
 # instance of logistic regression model
 log_reg = LogisticRegression()
@@ -50,12 +55,18 @@ log_reg = LogisticRegression()
 model = log_reg.fit(X_train, y_train)
 
 # make predictions
-y_pred = model.predict(X_test)
+predictions = model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
+# metrics
+accuracy = accuracy_score(y_test, predictions)
+precision = precision_score(y_test, predictions)
+recall = recall_score(y_test, predictions)
 
 print('Accuracy:', accuracy)
 print('Precision:', precision)
 print('Recall:', recall)
+
+plt.bar(['accuracy', 'precision', 'recall'], [accuracy, precision, recall])
+plt.xlabel('Metric')
+plt.ylabel('Score')
+plt.show()
